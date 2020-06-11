1fb9370480be9658635bfa0148d586ccaec3603c
Paul Holden <paul@holdensoftware.com>
2020-06-11 14:33:35 -0700

    Document account number tests

--- spec/models/licensees_spec.rb
+++ spec/models/licensees_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Licensee, type: :model do
-  describe 'validations' do
+  describe 'account number' do
     let(:account_id) { nil }
     let(:licensee) { create :licensee, account_id: account_id }
 
     shared_examples 'valid_account_number' do
+      # Licensee account numbers should match IFG's historical format of:
+      # A0000 where the number portion is padded to a width of at least 4.
+      # Numbers larger than this width will simply extend to as many
+      # characters as is needed.
       it 'has a valid Account Number format' do
         expect(licensee.account_number).to match(/\AA\d{4,}\z/)
       end
