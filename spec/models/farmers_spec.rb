968ffa67d1367b22cd383f9a43667053d5b9b562
Paul Holden <paul@holdensoftware.com>
2020-06-20 23:26:01 -0700

    Add Farmer/Field model tests

--- spec/models/farmers_spec.rb
+++ spec/models/farmers_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Farmer, type: :model do
+  let(:farmer) { create :farmer }
+
+  it 'has fields' do
+    5.times { create :field, farmer: farmer }
+    expect(farmer.fields).to all(be_a(Field))
+  end
 end
--- spec/models/fields_spec.rb
+++ spec/models/fields_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Field, type: :model do
+  let(:field) { create :field }
+
+  it 'has a farmer' do
+    expect(field.farmer).to be_a(Farmer)
+  end
 end
