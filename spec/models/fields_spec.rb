d9b43620e2c240950a4c928d73a3ee62e1eb407f
Paul Holden <paul@holdensoftware.com>
2020-06-24 13:35:12 -0700

    Add boundary area test for Fields

--- spec/models/fields_spec.rb
+++ spec/models/fields_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Field, type: :model do
   it 'has a farmer' do
     expect(field.farmer).to be_a(Farmer)
   end
+
+  it 'has an area' do
+    # The factory-returned object won't have the generated boundary area
+    # column, so we have to run an ActiveRecord query that will return it
+    # as part of the default scope for Fields.
+    expect(Field.find(field.id).boundary_area).to be_a(Numeric)
+  end
 end
