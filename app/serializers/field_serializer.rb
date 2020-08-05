dd220e72993b758c58fcb540410d7a72d511a59a
Paul Holden <paul@holdensoftware.com>
2020-08-05 07:48:21 -0700

    Hopefully fixes issue with WKT being mysteriously returned some encoded format

--- app/serializers/field_serializer.rb
+++ app/serializers/field_serializer.rb
@@ -1,9 +1,16 @@
 class FieldSerializer < ApplicationSerializer
   attribute :name
   attribute :srid
-  attribute :boundary
   attribute :created_at
   attribute :updated_at
+
+  attribute :boundary do
+    if object.boundary === String
+      object.boundary
+    else
+      object.boundary.as_text
+    end
+  end
 
   attribute :area do
     object.boundary_area
