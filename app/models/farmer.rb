199996f10042a8bbc765aa0fd4fea48b23220d37
Paul Holden <paul@holdensoftware.com>
2020-08-08 20:53:42 -0700

    Fix join in Farmer scope with field totals.
    Needs to be left join so Farmers with no fields are included

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -4,7 +4,7 @@ class Farmer < ApplicationRecord
 
   scope :with_field_totals, -> {
     includes(:fields).joins(<<-SQL
-      join (
+      left join (
         select farmers.id as farmer_id, count(fields.id) as count, sum(st_area(fields.boundary)) as area
         from farmers join fields on fields.farmer_id = farmers.id
         group by farmers.id
